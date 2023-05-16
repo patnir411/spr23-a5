9ffd0b0120ec7a66d9dff8895166a3eb651df4ac
Niral Patel <patnir41@gmail.com>
2023-05-16 01:41:55 -0700

    Makefile + tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 from calculator_adapter import run
 
+# Test addition
+assert run("4 + 7").output.strip() == "11", "Addition test failed"
 
-### ADD AT LEAST TWO TESTS HERE!
+# Test subtraction
+assert run("12 - 3").output.strip() == "9", "Subtraction test failed"
+
+print("All tests passed!")
 
 
 ###
